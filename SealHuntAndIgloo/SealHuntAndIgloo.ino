b8b609d733096d24ebb848f6656d13cd1ccd175b
BalinVanessa <vanashbalin@gmail.com>
2025-04-10 22:08:18 -0400

    Igloo and Motor Refactoring
    Removed the usage of delays. Renamed variables to better suit the context of the Igloo and Seal Hunt Motor

--- SealHuntAndIgloo/SealHuntAndIgloo.ino
+++ SealHuntAndIgloo/SealHuntAndIgloo.ino
@@ -5,24 +5,27 @@
 #include "SCMD_config.h" //Contains #defines for common SCMD register names and values
 #include "Wire.h"
 
-// MOTOR
-SCMD myMotorDriver; //This creates the main object of one motor driver and connected peripherals.
+// SEAL HUNT MOTOR VARIABLES
+SCMD sealHuntMotorDriver; //This creates the main object of one motor driver and connected peripherals.
 #define MOTOR_NUM 0
 
-// BREAKBEAM SENSOR CONSTANTS
+// IGLOO BREAKBEAM SENSOR CONSTANTS
 #define LEDPIN 13
-#define SENSORPIN 4
+#define IGLOO_SENSOR_PIN 4
 
-// LED CONSTANTS
-#define NUM_LEDS 30
-#define DATA_PIN 6
+// IGLOO LED CONSTANTS
+#define IGLOO_NUM_LEDS 30
+#define IGLOO_LED_PIN 6
+#define IGLOO_LED_DELAY 3000;
 
-// VARIABLES
-int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
+// IGLOO VARIABLES
+int iglooSensorState = 0, lastState=0;       
 bool triggerIglooLED = false;
+bool areIglooLightsOn = false;
+long timeToStopIglooLED;
 
 // Create a NeoPixel object
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel IGLOO_LED_STRIP = Adafruit_NeoPixel(IGLOO_NUM_LEDS, IGLOO_LED_PIN, NEO_GRB + NEO_KHZ800);
 
  
 void setup() {
@@ -32,16 +35,16 @@ void setup() {
 
   //***** Configure the Motor Driver's Settings *****//
   //  .commInter face is I2C_MODE 
-  myMotorDriver.settings.commInterface = I2C_MODE;
+  sealHuntMotorDriver.settings.commInterface = I2C_MODE;
 
   //  set address if I2C configuration selected with the config jumpers
-  myMotorDriver.settings.I2CAddress = 0x5D; //config pattern is "1000" (default) on board for address 0x5D
+  sealHuntMotorDriver.settings.I2CAddress = 0x5D; //config pattern is "1000" (default) on board for address 0x5D
 
   //  set chip select if SPI selected with the config jumpers
-  myMotorDriver.settings.chipSelectPin = 10;
+  sealHuntMotorDriver.settings.chipSelectPin = 10;
 
   //*****initialize the driver get wait for idle*****//
-  while ( myMotorDriver.begin() != 0xA9 ) //Wait until a valid ID word is returned
+  while ( sealHuntMotorDriver.begin() != 0xA9 ) //Wait until a valid ID word is returned
   {
     Serial.println( "ID mismatch, trying again" );
     delay(500);
@@ -50,82 +53,85 @@ void setup() {
 
   //  Check to make sure the driver is done looking for peripherals before beginning
   Serial.print("Waiting for enumeration...");
-  while ( myMotorDriver.ready() == false );
+  while ( sealHuntMotorDriver.ready() == false );
   Serial.println("Done.");
   Serial.println();
 
-  while ( myMotorDriver.busy() );
-  myMotorDriver.enable(); //Enables the output driver hardware
+  while ( sealHuntMotorDriver.busy() );
+  sealHuntMotorDriver.enable(); //Enables the output driver hardware
 
     // initialize the LED pin as an output:
   pinMode(LEDPIN, OUTPUT);      
   // initialize the sensor pin as an input:
-  pinMode(SENSORPIN, INPUT);     
-  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+  pinMode(IGLOO_SENSOR_PIN, INPUT);     
+  digitalWrite(IGLOO_SENSOR_PIN, HIGH); // turn on the pullup
 
-  strip.begin(); // Initialize NeoPixel strip
+  IGLOO_LED_STRIP.begin(); // Initialize NeoPixel strip
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //pass setDrive() a motor number, direction as 0(call 0 forward) or 1, and level from 0 to 255
-  myMotorDriver.setDrive( MOTOR_NUM, 0, 255);
+  sealHuntMotorDriver.setDrive( MOTOR_NUM, 0, 255);
 
   //IGLOO LED CODE
   // read the state of the pushbutton value:
-  sensorState = digitalRead(SENSORPIN);
+  iglooSensorState = digitalRead(IGLOO_SENSOR_PIN);
 
   // check if the sensor beam is broken
-  // if it is, the sensorState is LOW:
-  if (sensorState == LOW) {     
+  // if it is, the iglooSensorState is LOW:
+  if (iglooSensorState == LOW) {     
     // turn LED on:
     digitalWrite(LEDPIN, HIGH); 
-    triggerIglooLED = !triggerIglooLED;
-    updateLED();
+    turnOnIglooLED();
   } 
   else {
     // turn LED off:
     digitalWrite(LEDPIN, LOW); 
   }
   
-  if (sensorState && !lastState) {
+  // serial prints to check if the igloo breakbeam sensor has been broken or not
+  if (iglooSensorState && !lastState) {
     Serial.println("Unbroken");
   } 
-  if (!sensorState && lastState) {
+  if (!iglooSensorState && lastState) {
     Serial.println("Broken");
   }
-  lastState = sensorState;
+
+  lastState = iglooSensorState;
+  updateIglooLED();
 }
 
-void updateLED() {
-  if(triggerIglooLED) {
-    IglooLED();
+void updateIglooLED() {
+  if(areIglooLightsOn && millis() >= timeToStopIglooLED) {
+    turnOffIglooLED();
   }
-
-  delay(3000);
-  triggerIglooLED = false;
-  turnOffLED();
 }
 
-void IglooLED() {
+void turnOnIglooLED() {
+  areIglooLightsOn = true;
+
   // Set all LEDs to blue
-  for (int i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(0, 0, 255)); // Blue color
+  for (int i = 0; i < IGLOO_LED_STRIP.numPixels(); i++) {
+    IGLOO_LED_STRIP.setPixelColor(i, IGLOO_LED_STRIP.Color(0, 0, 255)); // Blue color
   }
 
   // Display the colors
-  strip.show(); // Update the display
+  IGLOO_LED_STRIP.show(); // Update the display
+  timeToStopIglooLED = millis() + IGLOO_LED_DELAY;
 }
 
-void turnOffLED()
+void turnOffIglooLED()
 {
+  areIglooLightsOn = false;
+
   // Set all LEDs to off
-  for (int i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(0, 0, 0)); // Blue color
+  for (int i = 0; i < IGLOO_LED_STRIP.numPixels(); i++) {
+    IGLOO_LED_STRIP.setPixelColor(i, IGLOO_LED_STRIP.Color(0, 0, 0)); // Blue color
   }
 
   // Display the colors
-  strip.show(); // Update the display
+  IGLOO_LED_STRIP.show(); // Update the display
 
 }
